debee25e4b405efdefa35a194148d1645c6a2e43
bala-amypo <bala@amypo.in>
2025-12-18 04:40:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -6,12 +6,3 @@ public class StudentRepository {
 
 
 
-
-package com.example.demo.service;
-
-import com.example.demo.entity.Student;
-
-public interface StudentService {
-
-    Student poststudent(Student st);
-}
